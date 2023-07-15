f6ba43b77d0b94cbd24f3c83d22a24ef1ad6d31d
HamedSY <hamed007.saboor@gmail.com>
2023-07-15 10:18:27 +0330

    Refactor the MakeADocumentListFromFiles tests and reduce its dependency

--- FullTextSearchTDD.Tests/DataReaderTest.cs
+++ FullTextSearchTDD.Tests/DataReaderTest.cs
@@ -26,21 +26,35 @@ public class DataReaderTest
     }
 
     [Theory]
-    [InlineData("58043",
-        @">This wouldn't happen to be the same thing as chiggers, would it>A truly awful parasitic affliction, as I understand it.  Tiny bug>dig deeply into the skin, burying themselves.  Yuck!  They have thes>things in OklahomaClose. My mother comes from Gainesville Tex, right across the borderThey claim to be the chigger capitol of the world, and I believe themWhen I grew up in Fort Worth it was bad enough, but in Gainesvillin the summer an attack was guaranteedDoug McDonal")]
-    [InlineData("59297",
-        @"HiThought I'd add something to the conversation.My girlfriend used to work in a lab studying different natural carcinogensShe mentioned once about the cancerous effect of barbecued foodBasically, she said that if you eat barbecued foods with strawberrie(a natural carcinogen) the slight carcinogenic properties of botcancel out each other-Jeff Pouporjtpoupor@undergrad.math.uwaterloo.c--Jeff Pouporjtpoupor@undergrad.math.uwaterloo.c")]
-    public void MakeADocumentListFromFiles_ShouldCreateACorrectList(string name, string content)
+    [MemberData(nameof(DocumentsData))]
+    public void MakeADocumentListFromFiles_ShouldCreateACorrectList(string name, string content,
+        IEnumerable<string> files)
     {
         // Arrange
         var dataReader = new DataReader();
         var document = new Document(name, content);
 
         // Act
-        var files = dataReader.ReadFilesFromADir(Path);
         var documents = dataReader.MakeADocumentListFromFiles(files);
 
         // Assert
         Assert.Contains(document, documents, new DocumentValueComparer());
+    }
+
+    public static IEnumerable<object[]> DocumentsData()
+    {
+        var files = new List<string> { Path + "\\58043", Path+ "\\59297" };
+        yield return new object[]
+        {
+            "58043",
+            @">This wouldn't happen to be the same thing as chiggers, would it>A truly awful parasitic affliction, as I understand it.  Tiny bug>dig deeply into the skin, burying themselves.  Yuck!  They have thes>things in OklahomaClose. My mother comes from Gainesville Tex, right across the borderThey claim to be the chigger capitol of the world, and I believe themWhen I grew up in Fort Worth it was bad enough, but in Gainesvillin the summer an attack was guaranteedDoug McDonal",
+            files
+        };
+        yield return new object[]
+        {
+            "59297",
+            @"HiThought I'd add something to the conversation.My girlfriend used to work in a lab studying different natural carcinogensShe mentioned once about the cancerous effect of barbecued foodBasically, she said that if you eat barbecued foods with strawberrie(a natural carcinogen) the slight carcinogenic properties of botcancel out each other-Jeff Pouporjtpoupor@undergrad.math.uwaterloo.c--Jeff Pouporjtpoupor@undergrad.math.uwaterloo.c",
+            files
+        };
     }
 }
